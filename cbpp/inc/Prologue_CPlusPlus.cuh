05a97ce02f81f350331683f5b24ee59f8f4d9c67
Don Williamson <dwilliamson_coder@hotmail.com>
2014-08-27 12:01:00 +0100

    ComputeBridge: New shareable Math header.

--- cbpp/inc/Prologue_CPlusPlus.cuh
+++ cbpp/inc/Prologue_CPlusPlus.cuh
@@ -48,82 +48,6 @@
 
 
 //
-// Basic type aliases
-//
-typedef unsigned char uchar;
-typedef unsigned short ushort;
-typedef unsigned int uint;
-typedef unsigned long ulong;
-
-
-//
-// Implementation of vector kernel types
-// Use macros instead of templates so that the types can be reflected
-//
-#define tuple2(type, name)							\
-	struct clcpp_attr(reflect_part) name			\
-	{												\
-		name() : x(0), y(0) { }						\
-		name(type x, type y) : x(x), y(y) { }		\
-		type x, y;									\
-	};
-#define tuple3(type, name)										\
-	struct clcpp_attr(reflect_part) name						\
-	{															\
-		name() : x(0), y(0), z(0) { }							\
-		name(type x, type y, type z) : x(x), y(y), z(z) { }		\
-		type x, y, z;											\
-	};
-#define tuple4(type, name)														\
-	struct clcpp_attr(reflect_part) name										\
-	{																			\
-		name() : x(0), y(0), z(0), w(0) { }										\
-		name(type x, type y, type z, type w) : x(x), y(y), z(z), w(w) { }		\
-		type x, y, z, w;														\
-	};
-
-tuple2(float, float2);
-tuple3(float, float3);
-tuple4(float, float4);
-
-tuple2(double, double2);
-tuple3(double, double3);
-tuple4(double, double4);
-
-tuple2(char, char2);
-tuple3(char, char3);
-tuple4(char, char4);
-
-tuple2(uchar, uchar2);
-tuple3(uchar, uchar3);
-tuple4(uchar, uchar4);
-
-tuple2(short, short2);
-tuple3(short, short3);
-tuple4(short, short4);
-
-tuple2(ushort, ushort2);
-tuple3(ushort, ushort3);
-tuple4(ushort, ushort4);
-
-tuple2(int, int2);
-tuple3(int, int3);
-tuple4(int, int4);
-
-tuple2(uint, uint2);
-tuple3(uint, uint3);
-tuple4(uint, uint4);
-
-tuple2(long, long2);
-tuple3(long, long3);
-tuple4(long, long4);
-
-tuple2(ulong, ulong2);
-tuple3(ulong, ulong3);
-tuple4(ulong, ulong4);
-
-
-//
 // Templated types to allow functions with texture parameters to compile.
 // This types are empty, meaning the functions can't be called from the host.
 //
@@ -133,6 +57,9 @@ template <typename> struct Texture2Du { };
 template <typename> struct Texture2Dn { };
 template <typename> struct Texture1Du { };
 template <typename> struct Texture1Dn { };
+
+
+#include "cbpp/Math.h"
 
 
 //
